0b9ec77a2880062356c0d2734997da247aa89c0f
kjustich <kjustich@users.noreply.github.com>
2017-02-24 09:49:11 -0500

    Add files via upload

---
+++ src/adventure/Rooms.java
@@ -0,0 +1,21 @@
+package adventure;
+
+import java.util.Scanner;
+
+public class Rooms 
+{
+	String name;
+	String description;
+	String[] contents = new String [3] ;
+	
+	public Rooms(String n, String d, String[] c)
+	{
+		name=n;
+		description=d;
+		contents=c;
+	}
+	
+	
+	
+	
+}
