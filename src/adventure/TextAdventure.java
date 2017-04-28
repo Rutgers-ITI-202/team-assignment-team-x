c09a5402ae9657e83ac6f1a2f10e8f761a476d6e
Spencer Yan <spenceryan123@gmail.com>
2017-04-28 13:02:40 -0400

    Added detailed descriptions for TextAdventure.java

--- src/adventure/TextAdventure.java
+++ src/adventure/TextAdventure.java
@@ -1,4 +1,3 @@
-
 package adventure;
 
 import java.util.Scanner;
@@ -8,33 +7,41 @@ import java.util.Scanner;
  * @version 1.0
  * @since 1.0
  * <h1> Description </h>
- * This is the main loop for the adventure text program it describes the first room and then asks if the user would like to play and then starts the game
+ * <p>Main game loop.</p>
+ * <p>param      player input, ask player if they want to begin game</p>
+ * <p>return     run the game
  *
  */
 public class TextAdventure {
-	
-	public static void main(String[] args) 		//Method that runs the main game
-	{
-		
-		
-		 AdventureModel runner=new AdventureModel();
-		runner.itemAdd();
-		runner.movement("start");
-		for(int x=0; x<runner.getMap().length;x++)
-		{
-			for(int y=0;y< runner.getMap()[0].length; y++)
-			{
-				System.out.print(runner.getMap()[x][y]);
-				System.out.print(" ");
-			}
-			System.out.println();
-				
-		}
-		while(true)
-		{
-			runner.command();
-		}
-	}
-	
+
+    public static void main(String[] args) {
+
+        // Main game loop.
+    	
+    	// Initialise new game runner.
+        AdventureModel runner = new AdventureModel();
+        
+        // Initialise new items.
+        runner.itemAdd();
+        
+        // Initialise player movement.
+        runner.movement("Start");
+        
+        // Initialise worl map.
+        for (int x = 0; x < runner.getMap().length; x++) {
+            for (int y = 0; y < runner.getMap()[0].length; y++) {
+                System.out.print(runner.getMap()[x][y]);
+                System.out.print(" ");
+            }
+            System.out.println();
+
+        }
+        
+        // Allow player to enter commands.
+        while (true) {
+            runner.command();
+        }
+    }
+
 
 }
